f30c5e2ca2e81f64fa128855311041c701d71361
monginadiana <dianahmongina2@gmail.com>
2022-10-28 09:47:13 +0300

    validations anderror handling

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,10 +5,12 @@ class MoviesController < ApplicationController
     movies = Movie.all
     render json: movies
   end
-
+  
   def create
-    movie = Movie.create(movie_params)
+    movie = Movie.create!(movie_params)
     render json: movie, status: :created
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
   end
 
   private
